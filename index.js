e0bef27ece0c87dad43084af699ddd9d555b9af4
ditpowuh <ditpowuh@gmail.com>
2019-10-28 20:49:25 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,85 @@
 module.exports = {
-
+  post: function(text) {
+    try {
+      console.log(text);
+    }
+    catch(err) {
+      return err;
+    }
+  },
+  delay: function(milliseconds) {
+    try {
+      var start = new Date().getTime();
+        for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds) {
+          break;
+        }
+      }
+    }
+    catch(err) {
+      return err;
+    }
+  },
+  length: function (string) {
+    try {
+      return string.length;
+    }
+    catch(err) {
+      return err;
+    }
+  },
+  popup: function(text) {
+    try {
+      alert(text);
+    }
+    catch(err) {
+      return err;
+    }
+  },
+  round: function(number) {
+    try {
+      return Math.round(number);
+    }
+    catch(err) {
+      return err;
+    }
+  },
+  type: function(input) {
+    try {
+      return typeof input;
+    }
+    catch(err) {
+      return err;
+    }
+  },
+  var: function(input1, input2) {
+    try {
+      if (!input1 || !input2) {
+        throw "AppleJuiceError: Input(s) needed (requires two)";
+      }
+      if (input1 != "string") {
+        throw "AppleJuiceError: First input incorrect type (string)";
+      }
+      if (typeof input2 == "number" || typeof input2 == "boolean" || typeof input2 == "object") {
+        eval(input1 + " = " + input2 + ";");
+      }
+      else if (typeof input2 == "string") {
+        eval(input1 + " = \"" + input2 + "\";");
+      }
+    }
+    catch(err) {
+      return err;
+    }
+  },
+  run: function(input) {
+    try {
+      if (!input) {
+        throw "AppleJuiceError: Input(s) needed (requires one)";
+      }
+      eval(input);
+    }
+    catch(err) {
+      return err;
+    }
+  }
 };
